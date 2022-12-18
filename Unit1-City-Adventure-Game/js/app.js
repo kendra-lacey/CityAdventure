dc14092e3948ad19366543bf86a7fd5d32cf9e6c
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2022-12-18 00:59:31 -0500

    add intro song

--- Unit1-City-Adventure-Game/js/app.js
+++ Unit1-City-Adventure-Game/js/app.js
@@ -1,6 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const titleEl= document.querySelector('.title')
+const introSong = new Audio ("../audio/intro.mp3")
+console.log(introSong)
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -11,7 +12,10 @@
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+titleEl.addEventListener("click",function(evt){
+  introSong.volume = .05
+  introSong.play()
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
