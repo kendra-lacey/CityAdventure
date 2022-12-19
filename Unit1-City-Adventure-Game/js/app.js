5756eb3ded53232515bcd700864c76076434544c
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2022-12-19 16:35:44 -0500

    add readMe

--- Unit1-City-Adventure-Game/js/app.js
+++ Unit1-City-Adventure-Game/js/app.js
@@ -1,23 +1,52 @@
 /*-------------------------------- Constants --------------------------------*/
-const titleEl= document.querySelector('.title')
 const introSong = new Audio ("../audio/intro.mp3")
-console.log(introSong)
+
+let gameBoardPics = [
+  "../images/gameboard/bedbathbeyond.jpeg",
+  "../images/gameboard/BODEGA.jpeg",
+  "../images/gameboard/brooklyn.jpeg",
+  "../images/gameboard/centralpark.jpeg",
+  "../images/gameboard/museum.jpeg",
+  "../images/gameboard/nypubliclibrary.jpeg",
+  "../images/gameboard/subway.jpeg",
+  "../images/gameboard/timessquare.jpeg",
+  "../images/gameboard/wholeFoods.jpeg"
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
-titleEl.parentNode.removeChild(titleEl);
+// titleEl.parentNode.removeChild(titleEl);
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const playBtn = document.getElementById('play-btn')
+console.log(playBtn)
+console.log(playBtn)
+const messageEl= document.querySelector('#message')
+const titleImageEl= document.querySelector('.title-img')
+console.log(titleImageEl)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-titleEl.addEventListener("click",function(evt){
+messageEl.addEventListener("click",function(evt){
   introSong.volume = .05
   introSong.play()
 })
 
+playBtn.addEventListener("click",function(evt){
+  messageEl.style.display = "none"
+})
 
 /*-------------------------------- Functions --------------------------------*/
 
+function init () {
+
+}
+
+function render () {
+
+}
+
+function handleClick (evt) {
+
+}
