2e59af9a881d4bb9b96b11537c6f08d51b69ca60
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2022-12-19 17:12:07 -0500

    cached element references

---
+++ Unit1-City-Adventure-Game/data/storyline.js
@@ -0,0 +1,11 @@
+const storyLine = {
+  {message:'Abbi has a tooth-ache and needs to get her wisdom teeth removed - but has no HeALTh inSurAnCe 😬'
+  },
+  {message: 'Go to Central Park and people watch'
+
+
+  }
+
+  
+
+}
--- Unit1-City-Adventure-Game/js/app.js
+++ Unit1-City-Adventure-Game/js/app.js
@@ -1,26 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 const introSong = new Audio ("../audio/intro.mp3")
 
-let gameBoardPics = [
-  "../images/gameboard/bedbathbeyond.jpeg",
-  "../images/gameboard/BODEGA.jpeg",
-  "../images/gameboard/brooklyn.jpeg",
-  "../images/gameboard/centralpark.jpeg",
-  "../images/gameboard/museum.jpeg",
-  "../images/gameboard/nypubliclibrary.jpeg",
-  "../images/gameboard/subway.jpeg",
-  "../images/gameboard/timessquare.jpeg",
-  "../images/gameboard/wholeFoods.jpeg"
-]
-
 /*---------------------------- Variables (state) ----------------------------*/
-// titleEl.parentNode.removeChild(titleEl);
 
 
 
 /*------------------------ Cached Element References ------------------------*/
 const playBtn = document.getElementById('play-btn')
-console.log(playBtn)
 console.log(playBtn)
 const messageEl= document.querySelector('#message')
 const titleImageEl= document.querySelector('.title-img')
@@ -28,14 +14,12 @@ console.log(titleImageEl)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-messageEl.addEventListener("click",function(evt){
+titleImageEl.addEventListener("click",function(evt){
   introSong.volume = .05
   introSong.play()
 })
 
-playBtn.addEventListener("click",function(evt){
-  messageEl.style.display = "none"
-})
+playBtn.addEventListener("click",init)
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -44,6 +28,7 @@ function init () {
 }
 
 function render () {
+  
 
 }
 
